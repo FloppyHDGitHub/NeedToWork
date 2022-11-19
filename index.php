3d0796d38e28bac89b0400bfad60e85a9da59726
Tim <timkoren@outlook.de>
2022-11-19 17:25:40 +0100

    Changed the look from the login

--- index.php
+++ index.php
@@ -110,32 +110,41 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body style="
 background-image: url('core/img/AtriumLogin.jpg');
+background-repeat: no-repeat;
+background-size: cover;
+background-color: black;
 ">
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
+    <div style="display: flex; justify-content: center;">
+        <div class="LoginBox">
+            <div class="BoxInner">
+                <div class="wrapper">
+                    <h2>Willkommen</h2>
+                    <p>Hier kannst du dich anmelden</p>
 
-        <?php
-        if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }
-        ?>
-
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                    <?php
+                    if(!empty($login_err)){
+                        echo '<div class="alert alert-danger">' . $login_err . '</div>';
+                    }
+                    ?>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="form-group">
+                            <label>Benutzername</label><br><br>
+                            <input type="text" name="username" class="unFill <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                            <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Passwort</label><br><br>
+                            <input type="password" name="password" class="pwFill <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                            <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                        </div>
+                        <div class="form-group"><br>
+                            <input type="submit" class="loginBtn" value="Anmelden">
+                        </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-        </form>
+        </div>
     </div>
+    </form>
+
 </body>
 </html>
